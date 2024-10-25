166934f4e9887a67e0f8781438ec22209afbe5fb
Jorr910 <00166721@uca.edu.sv>
2024-10-25 13:44:37 -0600

    :sparkles: Login and Register

---
+++ src/assets/pages/login/Login.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import "./Login.css"
+import logo from "../../img/kodigo_logo.png"
+
+
+
+export const Login = () => {
+
+
+
+  return (
+    <div>
+      <form id='container'>
+
+      <div> 
+
+      <img id='img_logo' src= {logo} alt='Kodigo_logo'/>
+      
+      
+      </div>
+
+
+      {/* IMPUT DATA AND BTN*/}
+      
+        
+        <input type='text' id='Email_User' placeholder='Nombre de usuario'  />
+    
+        <input type='password' id='Password_User' placeholder='Contraseña'  />
+        
+        <button type='submit' id='acceso'>Acceder</button>
+        <button type='submit' id='New_user'>Nuevo usuario</button>
+
+
+        {/*CONFIG USER*/}
+
+        <div id='Container_Mod'>
+
+          <a href='#' id='Mod_User'>¿Olvidaste tu Contraseña?</a>
+
+          <a href='#'>Aviso de cookies</a>
+
+        </div>
+
+
+      </form>
+    </div>
+  )
+}
